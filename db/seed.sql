b97f4dd3d29dfb27eec4c5fc892b3ef05733ae8d
Arlolic <41295998+Arlolic@users.noreply.github.com>
2021-01-25 19:06:16 -0500

    Update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -5,7 +5,7 @@ VALUES
   ('Biology', 'Watson', 90000),
   ('Comp. Sci.', 'Taylor', 100000),
   ('Elec. Eng.', 'Taylor', 85000),
-  ('Finance', 'Painter', 120000),
+  ('Economy', 'Painter', 120000),
   ('History', 'Painter', 50000),
   ('Music', 'Packard', 80000),
   ('Physics', 'Watson', 70000);
